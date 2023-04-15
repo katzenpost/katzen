d540cd845a69c80663964dd735b165270097fe32
Emery Hemingway <ehmry@posteo.net>
2023-04-15 10:17:19 -0500

    Do not build from i686-linux
    Some crypto libraries missing for 32-bit x86.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       legacyPackages = lib.attrsets.mapAttrs (system: pkgs:
         pkgs.appendOverlays (builtins.attrValues self.overlays)) {
           inherit (nixpkgs.legacyPackages)
-            i686-linux x86_64-linux aarch64-linux;
+            x86_64-linux aarch64-linux;
         };
 
       packages = lib.attrsets.mapAttrs (system: pkgs: rec {
