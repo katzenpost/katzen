f148dd6bcfa5bb6671ce7af2a03511def493c270
Masala <masala@riseup.net>
2024-10-05 20:05:24 +0000

    don't strp Sender or Receiver metadata...

--- message.go
+++ message.go
@@ -29,13 +29,13 @@ type Message struct {
 	ID uint64
 
 	// sender Contact.ID for this client
-	Sender uint64 `cbor:"-"`
+	Sender uint64
 
 	// Sent is the sender timestamp
 	Sent time.Time
 
 	// Received is the reciver timestamp
-	Received time.Time `cbor:"-"`
+	Received time.Time
 
 	// Acked is when the receiver acknowledged this message
 	Acked time.Time
