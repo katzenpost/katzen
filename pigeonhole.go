6166cb90939819d9fabf9c5e0a930e8b5e329935
Masala <masala@riseup.net>
2025-04-17 05:31:33 +0000

    wip file transfer

---
+++ pigeonhole.go
@@ -0,0 +1,284 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"gioui.org/layout"
+	"github.com/katzenpost/hpqc/bacap"
+	"github.com/katzenpost/hpqc/rand"
+	"github.com/katzenpost/katzenpost/client"
+	"github.com/katzenpost/katzenpost/core/worker"
+	mClient "github.com/katzenpost/katzenpost/pigeonhole/client"
+	"golang.org/x/crypto/blake2b"
+	"io"
+	"os"
+	"sync"
+	"time"
+
+	//"gioui.org/x/explorer"
+	"gioui.org/widget"
+)
+
+// Downloader
+type Downloader struct {
+	worker.Worker
+	transport mClient.ReadWriteClient
+	db        *BadgerStore
+
+	// State is the State of the Downloader
+	State DownloadState
+
+	// Header contains metadata Name, Sum256, Size
+	Header *DownloadHeader
+
+	// Sum256 is the current hash.Hash.Sum256 of the bytes received
+	Sum256 []byte
+
+	// io.Writer is where received bytes get written
+	dest io.Writer
+
+	// each upload or download consists of a number of chunks
+	// which are stored with a uint64 key in a db
+	Chunks []uint64
+
+	// Number of read Chunks
+	BytesRead uint64
+
+	// We need a structure to hold a reference to each payload response
+	// that we received from pigeonhole in our database
+	// so that
+
+	// UI buttons associated with each Downloader
+	startBtn  *widget.Clickable
+	cancelBtn *widget.Clickable
+	deleteBtn *widget.Clickable
+
+	cmdCh chan Command
+}
+
+// NewUploadHeader returns an UploadHeader created for the File
+func NewUploadHeader(path string) (*UploadHeader, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+	ownerCap, err := bacap.NewBoxOwnerCap(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	if fileInfo.Size() <= 0 {
+		return nil, fmt.Errorf("Empty or System file: %s", fileInfo.Name())
+	}
+	sum, err := sum256fileInfo(path)
+	if err != nil {
+		return nil, err
+	}
+	header := &UploadHeader{WriteCap: ownerCap,
+		Name:   fileInfo.Name(),
+		Length: uint64(fileInfo.Size()),
+		Sum256: sum,
+	}
+
+	return header, nil
+}
+
+// NewDownloader returns a new Downloader with the
+func NewUploader(transport *mClient.Client, db *BadgerStore, h *UploadHeader, source io.Reader) (*Uploader, error) {
+	// XXX: set db
+	ul := &Uploader{startOnce: new(sync.Once), Header: h, db: db, source: source, startBtn: new(widget.Clickable), cancelBtn: new(widget.Clickable), deleteBtn: new(widget.Clickable)}
+	return ul, nil
+}
+
+func NewDownloader(session *client.Session, h *DownloadHeader, dest io.Writer) *Downloader {
+	dloader := &Downloader{State: Proposed,
+		Header: h, dest: dest, cmdCh: make(chan Command),
+		startBtn:  &widget.Clickable{},
+		cancelBtn: &widget.Clickable{},
+		deleteBtn: &widget.Clickable{},
+	}
+	return dloader
+}
+
+// bacapworker receives command to start/stop reading and storing bytes
+func (d *Downloader) worker() {
+	reader, err := bacap.NewStatefulReader(d.Header.ReadCap, d.Header.Sum256)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		select {
+		case <-d.HaltCh():
+			return
+		case cmd := <-d.cmdCh:
+			switch cmd {
+			case Start:
+			case Stop:
+				return
+			default:
+			}
+		default:
+
+		}
+		// is already read
+		if d.BytesRead == d.Header.Length {
+			// set state to complted
+			return
+		}
+		boxID, err := reader.NextBoxID()
+		if err != nil {
+			return
+		}
+		// XXX: create a context that times out after some delay
+		ctx := context.Background()
+
+		// fetch the box
+		// XXX: FIXME: obtain signature along with payload fromt he client; requires api change
+		sig := [64]byte{'f', 'm', 'l'}
+		ciphertext /*sig,*/, err := d.transport.GetWithContext(ctx, boxID.Bytes())
+		if err != nil {
+			// retry
+			continue
+		}
+		// obtain plaintext. a decryption failure is a fatal error
+		plaintext, err := reader.DecryptNext(d.Header.Sum256, boxID.ByteArray(), ciphertext, sig)
+		if err != nil {
+			// XXX: log or handle this error
+			return
+		}
+
+		// write bytes to dest. failure to write is a fatal error
+		n, err := d.dest.Write(plaintext)
+		if err != nil || n != len(plaintext) {
+			// XXX: log or handle this error
+			panic(err)
+			return
+		}
+
+		// update how many bytes have been read
+		d.BytesRead += uint64(len(plaintext))
+	}
+}
+
+var (
+	downloadList = &layout.List{Axis: layout.Vertical}
+	uploadList   = &layout.List{Axis: layout.Vertical}
+)
+
+// DownloadState indicates the state of the Downloader
+// it has state proposed, rejected, inprogress, completed
+// a new Downlaod starts in state Proposed, and proceeds to Rejected or InProgress state
+// on starting, and reaches state Completed or Failed
+type DownloadState uint8
+
+const (
+	Proposed DownloadState = iota
+	Rejected
+	InProgress
+	Completed
+	Failed
+)
+
+type Uploader struct {
+	startOnce *sync.Once
+	worker.Worker
+
+	Header    *UploadHeader
+	db        *BadgerStore
+	source    io.Reader
+	transport mClient.ReadWriteClient
+
+	// Total bytes written
+	BytesWritten uint64
+
+	// Chunk IDs of the Chunks created
+	Chunks []uint64
+
+	startBtn  *widget.Clickable
+	cancelBtn *widget.Clickable
+	deleteBtn *widget.Clickable
+}
+
+func (u *Uploader) Start() {
+	u.startOnce.Do(func() {
+		u.Go(u.worker)
+		<-u.HaltCh()
+		u.startOnce = new(sync.Once)
+	})
+}
+
+func (u *Uploader) worker() {
+	writer, err := bacap.NewStatefulWriter(u.Header.WriteCap, u.Header.Sum256)
+	if err != nil {
+		panic(err)
+	}
+	payloadSize := u.transport.PayloadSize()
+
+	for u.BytesWritten < u.Header.Length {
+		buf := make([]byte, payloadSize)
+		n, err := io.ReadFull(u.source, buf)
+		switch err {
+		case nil, io.ErrUnexpectedEOF:
+			// XXX: does EncryptNext do padding?
+			box_id, ciphertext, sig, err := writer.EncryptNext(buf[:n])
+			if err != nil {
+				panic(err)
+			}
+			// create and save the Chunk locally
+			ch := &Chunk{ID: rand.NewMath().Uint64(), Key: box_id, Payload: ciphertext}
+			copy(ch.Signature[:], sig)
+			err = u.db.PutChunk(ch)
+			if err != nil {
+				panic(err)
+			}
+			u.Chunks = append(u.Chunks, ch.ID)
+			u.BytesWritten += uint64(len(ciphertext))
+
+			ctx, cancelFn := context.WithTimeout(context.Background(), time.Minute)
+			//err = u.transport.PutWithContext(ctx, box_id[:], sig[:], ciphertext)
+			err = u.transport.PutWithContext(ctx, box_id[:], ciphertext)
+			cancelFn()
+			if err != nil {
+				panic(err)
+				// Chunk should have a flag on whether it has been uploaded
+			}
+		default:
+			panic(err)
+		}
+	}
+}
+
+func sum256fileInfo(path string) ([]byte, error) {
+	r, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	h, err := blake2b.New256(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the block size and make a buffer a multiple thereof
+	blockSize := h.BlockSize()
+	buf := make([]byte, blockSize*1024)
+
+	// Read from the file and write to the hash
+	for {
+		n, err := r.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				// Write the remaining bytes to the hash
+				h.Write(buf[:n])
+				break
+			}
+			return nil, err
+		}
+
+		// Write the buffer to the hash
+		h.Write(buf[:n])
+	}
+
+	// Get the hash sum
+	return h.Sum(nil), nil
+}
---
+++ transfer.go
@@ -0,0 +1,262 @@
+package main
+
+import (
+	"encoding/base64"
+	"gioui.org/io/key"
+	"gioui.org/layout"
+	"github.com/katzenpost/hpqc/bacap"
+	"github.com/katzenpost/hpqc/sign/ed25519"
+	mClient "github.com/katzenpost/katzenpost/pigeonhole/client"
+	"golang.org/x/exp/shiny/materialdesign/icons"
+	"os"
+
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+var (
+	startIcon, _  = widget.NewIcon(icons.AVPlayArrow)
+	stopIcon, _   = widget.NewIcon(icons.NavigationCancel)
+	deleteIcon, _ = widget.NewIcon(icons.ActionDelete)
+)
+
+// UploadHeader contains the metadata for uploading a file and producing a DownloadHeader
+type UploadHeader struct {
+	WriteCap *bacap.BoxOwnerCap
+	Name     string
+	Length   uint64
+	Sum256   []byte
+}
+
+// DownloadHeader holds the Read capability and file metadata
+type DownloadHeader struct {
+	ReadCap *bacap.UniversalReadCap // the read capability
+	Name    string                  // the name of the Payload
+	Length  uint64                  // the length of the Payload
+	Sum256  []byte                  // the hash of the Payload
+}
+
+// DownloadHeader returns the Header used to start a download
+func (u *UploadHeader) DownloadHeader() *DownloadHeader {
+	d := new(DownloadHeader)
+	d.ReadCap = u.WriteCap.UniversalReadCap()
+	d.Name = u.Name
+	d.Length = u.Length
+	d.Sum256 = u.Sum256
+	return d
+}
+
+// Chunk holds each bacap message
+type Chunk struct {
+	ID        uint64
+	Key       [ed25519.PublicKeySize]byte
+	Signature [ed25519.SignatureSize]byte
+	Payload   []byte
+	Sent      bool
+}
+
+// TransferPage tracks the uploads and downloads in progress and provides UI to stop/start/cancel transfers
+// It consists of the navbar and two lists, one for uploads and one for downloads.
+// Each element of these lists present controls for (re) starting / stopping / removing a transfer
+type TransferPage struct {
+	a         *App
+	downloads []*Downloader
+	uploads   []*Uploader
+	add       *widget.Clickable
+	back      *widget.Clickable
+	updateCh  chan struct{}
+}
+
+type TransferCancelled struct{}
+type TransferCompleted struct{}
+type TransferRemoved struct{}
+
+func (t *TransferPage) Start(stop <-chan struct{}) {
+	go func() {
+		for {
+			select {
+			case <-stop:
+				return
+			case <-t.updateCh:
+			}
+		}
+	}()
+}
+
+// Update is called by the Chooser with the path chosen
+func (t *TransferPage) Update(item interface{}) {
+	switch i := item.(type) {
+	case string:
+		uh, err := NewUploadHeader(i)
+		if err != nil {
+			return
+		}
+		f, err := os.Open(i)
+		if err != nil {
+			return
+		}
+		// XXX: requires a non nil session (ie, be online)
+		// in order to create an Uploader, which is lame.
+		t.a.Session()
+		transport, err := mClient.NewClient(t.a.Session())
+		if err != nil {
+			return
+		}
+		dl, err := NewUploader(transport, t.a.db, uh, f)
+		if err != nil {
+			return
+		}
+		t.uploads = append(t.uploads, dl)
+	}
+}
+
+func newTransferPage(a *App) *TransferPage {
+	t := &TransferPage{a: a,
+		uploads:   []*Uploader{},
+		downloads: []*Downloader{},
+		add:       &widget.Clickable{},
+		back:      &widget.Clickable{},
+		updateCh:  make(chan struct{}, 1),
+	}
+	return t
+}
+
+// Layout the view for a Downloader
+func (d *Downloader) Layout(gtx layout.Context) layout.Dimensions {
+	progress := float32(d.BytesRead) / float32(d.Header.Length)
+	return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceBetween, Alignment: layout.Middle}.Layout(gtx,
+		layout.Rigid(material.Caption(th, d.Header.Name).Layout),
+		layout.Rigid(material.ProgressBar(th, progress).Layout),
+
+		layout.Rigid(func(gtx C) D {
+			return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween, Alignment: layout.Middle}.Layout(
+				gtx,
+				layout.Rigid(button(th, d.startBtn, startIcon).Layout),
+				layout.Rigid(button(th, d.cancelBtn, cancelIcon).Layout),
+				layout.Rigid(button(th, d.deleteBtn, deleteIcon).Layout),
+			)
+		}),
+	)
+}
+
+// Layout the view for a Uploader
+// Shown is the file name and size, a progress bar, and start/stop
+func (u *Uploader) Layout(gtx layout.Context) layout.Dimensions {
+	progress := float32(u.BytesWritten) / float32(u.Header.Length)
+	return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceBetween, Alignment: layout.Middle}.Layout(gtx,
+		layout.Rigid(material.Caption(th, u.Header.Name).Layout),
+		layout.Rigid(material.Caption(th, base64.StdEncoding.EncodeToString(u.Header.Sum256)).Layout),
+		layout.Rigid(material.ProgressBar(th, progress).Layout),
+		layout.Rigid(func(gtx C) D {
+			return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween, Alignment: layout.Middle}.Layout(
+				gtx,
+				layout.Rigid(button(th, u.startBtn, startIcon).Layout),
+				layout.Rigid(button(th, u.cancelBtn, cancelIcon).Layout),
+				layout.Rigid(button(th, u.deleteBtn, deleteIcon).Layout),
+			)
+		}),
+	)
+}
+
+// after the download is complete the buttons change to close
+//
+// each upload list item laid out has the file name and size, a progress bar, and cancel / repair / delete buttons
+// clicking delete removes the attachment from the list
+// Event checks to see if any of the UX elements have changed and does the corresponding actions
+func (t *TransferPage) Event(gtx layout.Context) interface{} {
+	if t.add.Clicked(gtx) {
+		return NewChooser{}
+	}
+
+	if t.back.Clicked(gtx) {
+		return BackEvent{}
+	}
+
+	// handle keyboard shortcuts
+	if e, ok := shortcutEvents(gtx); ok {
+		switch e.Name {
+		case key.NameEscape:
+			panic("wtf")
+			return BackEvent{}
+		}
+
+	}
+
+	// check if uploader buttons are clicked and do the action
+	for i, ul := range t.uploads {
+		if ul.startBtn.Clicked(gtx) {
+			ul.Start()
+			return nil
+		}
+		if ul.cancelBtn.Clicked(gtx) {
+			ul.Halt()
+			return TransferCancelled{}
+		}
+		if ul.deleteBtn.Clicked(gtx) {
+			ul.Halt()
+			t.uploads = append(t.uploads[:i], t.uploads[i+1:]...)
+			return TransferRemoved{}
+		}
+		if ul.BytesWritten == ul.Header.Length {
+			ul.Halt()
+			return TransferCompleted{}
+		}
+	}
+	// check if any downloader buttons are clicked
+	for i, dl := range t.downloads {
+		if dl.cancelBtn.Clicked(gtx) {
+			dl.Halt()
+		}
+		if dl.deleteBtn.Clicked(gtx) {
+			dl.Halt()
+			t.downloads = append(t.downloads[:i], t.downloads[i+1:]...)
+		}
+		if dl.BytesRead == dl.Header.Length {
+			// all of the source bytes have been read
+			// and we can now produce the Sum256 of the file
+			// XXX: do that
+		}
+	}
+	return nil
+}
+
+func (t *TransferPage) Layout(gtx layout.Context) layout.Dimensions {
+	return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceBetween, Alignment: layout.Middle}.Layout(gtx,
+		// layout back button, title
+		layout.Rigid(func(gtx C) D {
+			return bgList.Layout(gtx, func(gtx C) D {
+				return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween, Alignment: layout.Middle}.Layout(gtx,
+					layout.Rigid(button(th, t.back, backIcon).Layout),
+					layout.Rigid(material.Caption(th, "Transfer Page").Layout),
+					layout.Flexed(1, fill{th.Bg}.Layout),
+				)
+			},
+			)
+		}),
+		// layout upload list
+		layout.Flexed(2, func(gtx C) D {
+			return bgList.Layout(gtx, func(ctx C) D {
+				// return empty if there are no messages
+				if len(t.uploads) == 0 {
+					return fill{th.Bg}.Layout(ctx)
+				}
+				return uploadList.Layout(gtx, len(t.uploads), func(gtx C, i int) D {
+					return t.uploads[i].Layout(gtx)
+				})
+			})
+		}),
+
+		// layout download list
+		layout.Flexed(2, func(gtx C) D {
+			return bgList.Layout(gtx, func(ctx C) D {
+				// return empty if there are no messages
+				if len(t.downloads) == 0 {
+					return fill{th.Bg}.Layout(ctx)
+				}
+				return downloadList.Layout(gtx, len(t.downloads), func(gtx C, i int) D {
+					return t.downloads[i].Layout(gtx)
+				})
+			})
+		}),
+	)
+}
